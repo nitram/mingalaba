aac55f033a41591fe6a7180f7a45d2b28da1c600
Martin <martin.sayhello@gmail.com>
2024-03-15 15:38:36 -0700

    first commit

---
+++ mingalaba/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-nb%_*^%&l+xig%dyh*pxg)0tu5a_pvl2w_d3o0b-4#xk2=q42^"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
